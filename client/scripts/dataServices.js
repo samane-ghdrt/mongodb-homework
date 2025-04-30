851afb30289ca11b7b184eab60c566d824e53bc7
samane <samane.ghodrati@gmail.com>
2025-04-30 03:37:39 -0700

    add

---
+++ client/scripts/dataServices.js
@@ -0,0 +1,9 @@
+const baseURL = ""; // Write down the base URL of the created API.
+
+const getAllUser = async () => {};
+
+const createUser = async (userData) => {};
+
+const deleteUser = async (id) => {};
+
+export { getAllUser, deleteUser, createUser };
