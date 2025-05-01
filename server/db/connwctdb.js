d32876704d420fcb66b396432b36f8424ebd2621
samane <samane.ghodrati@gmail.com>
2025-05-01 02:54:38 -0700

    add

---
+++ server/db/connwctdb.js
@@ -0,0 +1,6 @@
+const mongoose =require("mongoose");
+
+const connectDb=(uri)=>{
+    return mongoose.connect(uri);
+};
+module.exports={connectDb};
--- server/rout.js
+++ server/rout.js
@@ -1,4 +1,19 @@
 const express=require('express');
 const rout=express.Router();
 
-rout.get()
+const {connectDb}=require('./db/connwctdb')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+ 
+rout.get();
+
+const start=async()=>{
+    try {
+        await connectDb(process.env.)
+   ap
+   
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
